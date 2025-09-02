b0273717a211894317f885f77bd14a8503907900
VictorOrduz <victicordux@gmail.com>
2025-09-01 21:45:40 -0500

    insersion de comentarios en el codigo del archivo circuloCon#.py

--- circuloCon#.py
+++ circuloCon#.py
@@ -3,14 +3,16 @@
 dibujar un circulo usando el simbolo #
 """
 
+# radio del circulo = 4
 r = 4
 
-for x in range(-r, r + 1):
-    for y in range(-r, r + 1):
-        
+for x in range(-r, r + 1): # recorre las coordenadas desde -4 hasta 4
+    for y in range(-r, r + 1): # recorre las coordenadas desde -4 hasta 4
+
+        # condicion matematica; formula del circulo
         if x * x + y * y <= r * r:
-            print(" # ", end = "")
+            print(" # ", end = "") # si cummple la condicion matematica, imprime # dentro y en el limte del circulo
         else:
-            print(" . ", end = "")
+            print("   ", end = "") # si no cumple la condicion matematica, imprime espacio fuera del circulo
             
     print()
